9064ffd563695edd4924a8281153017f8b9393f5
doughn0 <doughnut1946@gmail.com>
2023-02-04 22:21:47 +0100

    improvements

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -21,9 +21,9 @@ function Tile({x, y, blockSize=1, active, setActive, id, children}) {
                 onClick={() => setActive(act ? "" : id)}
                 className={(hid ? "hidden " : "") + (act ? "active " : "") + "tile"}
                 style={!act ? 
-                    { top: top+'px', left: left+'px', height: height+'px', width: width+'px', fontSize: height+'px'}
+                    { top: top+'px', left: left+'px', height: height+'px', width: width+'px', fontSize: (width+height)+'px'}
                     :
-                    { top: (fromTop-10)+'px', left: (fromLeft-50)+'px', height: (maxHeight+20)+'px', width: (maxWidth+100)+'px', fontSize: maxHeight+'px' }
+                    { top: (fromTop-10)+'px', left: (fromLeft-50)+'px', height: (maxHeight+20)+'px', width: (maxWidth+100)+'px', fontSize: (maxHeight+maxWidth)+'px' }
                 }
             >
                 <div style={{ 
